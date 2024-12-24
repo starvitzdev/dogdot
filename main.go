a99bccd02287dc0bd51f30563ed7e840187ed0cd
starvitzdev <starvitz@pm.me>
2024-12-25 00:29:17 +0100

    At least i started something today... mby tommorow

--- main.go
+++ main.go
@@ -7,16 +7,24 @@ import (
 )
 
 func main() {
+	url := "https://www.scrapethissite.com/pages/simple/"
+	countries := []string{}
+	newCountries := []string{}
+
+	fmt.Println("starting main function")
+
 	c := colly.NewCollector()
 
-	// Find and visit all links
-	c.OnHTML("a", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
+	c.OnHTML(".country-name", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
+		countries = append(countries, e.Text)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	c.Visit("https://folkspel.se")
+	c.Visit(url)
+
+	fmt.Println(newCountries)
 }
